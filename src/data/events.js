500b3e3176371fcb31348c4be222968cbc03e5cf
Marco Bruno <marco.bruno.br@gmail.com>
2020-03-29 19:47:51 +0200

    :sparkles: Adicionado um monte de cursos, apostilas e tudo mais

--- src/data/events.js
+++ src/data/events.js
@@ -5,7 +5,7 @@ export default [
     alt: "Logo da CollabCode com o mascote Gueio",
     content:
       "Toda semana um desafio novo para você fazer, com aulas free e ao vivo pela Twitch e YouTube às 19hrs. É grátis de verdade, não será vendido nada!",
-    url: "http://quarentena.tech/collabcode",
+    url: "https://twitch.tv/collabdode",
     className: ""
   },
   {
@@ -27,6 +27,24 @@ export default [
     className: "-radius"
   },
   {
+    title: "#ApostilasFree",
+    logo: "/img/caelum.png",
+    alt: "Logo da Caelum",
+    content:
+      "Toda semana um desafio novo para você fazer, com aulas free e ao vivo pela Twitch e YouTube às 19hrs. É grátis de verdade, não será vendido nada!",
+    url: "https: //www.caelum.com.br/apostilas",
+    className: "-radius"
+  },
+  {
+    title: "#hackForGood",
+    logo: "/img/shawee.jpg",
+    alt: "Logo da Shawee achei top",
+    content:
+      "A Shawee não poderia deixar de lado o principal assunto do país e do mundo atualmente: o Coronavírus. Em parceria com a Twilio...",
+    url: "https://hack-for-good.shawee.io",
+    className: "-radius"
+  },
+  {
     title: "#RobotWeek",
     logo: "/img/roboweek.svg",
     alt: "Cabeça do Robo Week",
@@ -45,11 +63,168 @@ export default [
     className: "-radius"
   },
   {
-    title: "",
-    logo: "",
-    alt: "",
-    content: "",
-    url: "",
+    title: "#MegaHack",
+    logo: "/img/shawee.jpg",
+    alt: "Logo da Shawee achei top",
+    content:
+      "Mega Hack COVID-19 é uma maratona criativa que tem objetivo de criar um grande banco com projetos reais e ideias...",
+    url: "https://megahackcovid19economia.shawee.io",
+    className: "-radius"
+  },
+  {
+    title: "#Qarentena",
+    logo: "/img/Iterasys.jpg",
+    alt: "Logo do Iterasys",
+    content:
+      "Novas vagas de Teste e QA continuam sendo abertas todos os dias para trabalhar remotamente. Quantas profissões podem dizer o mesmo nesse momento?",
+    url: "https://qarentena.com.br/",
+    className: "-radius"
+  },
+  {
+    title: "#CoronaKindle",
+    logo: "/img/codigo-falado.png",
+    alt: "Logo do Código Falado",
+    content:
+      "Lançado o Desafio Relâmpago #CoronaKindle para limpar a internet do coronavírus! Você tem uma semana para desenvolver uma extensão de Firefox ou Chrome. Envie o link da sua extensão com instruções de como testar neste tweet. Mais detalhes no vídeo. Sorteio será no sábado 04/04",
+    url: "https://twitter.com/CodigoFalado/status/1244296923424702465",
+    className: "-radius"
+  },
+  {
+    title: "#Loiane",
+    logo: "/img/loiane.jpg",
+    alt: "Foto da Loiane",
+    content:
+      "Cursos totalmente grátis de Java, Angular, Cordova e mais, com ninguém menos que a Loiane! ",
+    url: "https://loiane.training",
+    className: "-radius"
+  },
+  {
+    title: "#lumina",
+    logo: "/img/lumina.png",
+    alt: "Logo da Lúmina",
+    content:
+      " Cursos online e gratuitos com certificado topezera para você não ver o tempo passar!",
+    url: "https://lumina.ufrgs.br/",
+    className: "-radius"
+  },
+  {
+    title: "#desenvolvedor.io",
+    logo: "/img/eduardo-pires.png",
+    alt: "Foto feliz do Eduardo Pires",
+    content:
+      "Eduardo Pires é desenvolvedor, arquiteto, instrutor, palestrante, pai do Enzo e fundador do desenvolvedor.io que tem uns curso free e feliz",
+    url:
+      "https://desenvolvedor.io/curso-online-fundamentos-de-arquitetura-de-software",
+    className: "-radius"
+  },
+  {
+    title: "#TalkDasMinas",
+    logo: "/img/talk-das-minas.jpg",
+    alt: "Logo do Talk das minas",
+    content:
+      "Você não precisa ficar sozinha(o) nessa quarentena! A gente tem uma programação incrível pra você acompanhar 💙 Todo dia um tema diferente via Hangouts",
+    url:
+      "https://desenvolvedor.io/curso-online-fundamentos-de-arquitetura-de-software",
+    className: "-radius"
+  },
+  {
+    title: "#CursoEmVideo",
+    logo: "/img/curso-em-video.png",
+    alt: "Logo do Curso em Vídeo",
+    content:
+      "Aulas 100% ao vivo de Git e GitHub do Guanabara, é de graça e feliz!",
+    url:
+      "https://www.youtube.com/playlist?list=PLHz_AreHm4dm7ZULPAmadvNhH6vk9oNZA",
+    className: "-radius"
+  },
+  {
+    title: "#PentestFree",
+    logo: "/img/desec.jpg",
+    alt: "Logo da Desec",
+    content:
+      "A Primeira versão do curso de Pentest Profissional não será mais comercializada, então queremos tornar público o acesso das aulas para todos que tem paixão por segurança ofensiva poder usufruir da melhor e mais aceita metodologia adotada na America Latina. ",
+    url: "https://desecsecurity.com/curso/curso-pentest-gratuito",
+    className: "-radius"
+  },
+  {
+    title: "#NodeBR",
+    logo: "/img/nodebr.jpg",
+    alt: "Logo da NodeBR",
+    content:
+      "A Primeira versão do curso de Pentest Profissional não será mais comercializada, então queremos tornar público o acesso das aulas para todos que tem paixão por segurança ofensiva poder usufruir da melhor e mais aceita metodologia adotada na America Latina. ",
+    url: "https://treinamento.nodebr.org",
+    className: "-radius"
+  },
+  {
+    title: "#PythonParaZumbis",
+    logo: "/img/python-para-zumbis.jpg",
+    alt: "Python para Zumbis",
+    content:
+      "Curso de Python para iniciantes Zumbis, cola aí que o conteúdo está sendo atualizado :-)",
+    url:
+      "https://www.youtube.com/playlist?list=PLUukMN0DTKCtbzhbYe2jdF4cr8MOWClXc",
+    className: "-radius"
+  },
+  {
+    title: "#AprendaReact",
+    logo: "/img/react.png",
+    alt: "Aprenda React sem logo então coloquei o logo do React",
+    content:
+      "Neste curso você vai aprender os básicos de React e evoluir passo-a-passo até conseguir publicar sua primeira aplicação em React.js",
+    url: "https://www.aprendareact.com.br/cursos/iniciando-em-react",
+    className: ""
+  },
+  {
+    title: "#DevMedia",
+    logo: "/img/devmedia.png",
+    alt: "Logo da DevMedia",
+    content:
+      "Neste curso de CSS conheça como importar e incorporá-lo no HTML. Além disso você aprenderá a escrever seus primeiros códigos e conhecer as principais propriedades e atributos",
+    url: "https://www.devmedia.com.br/curso/curso-css-gratuito/2215",
+    className: ""
+  },
+  {
+    title: "#AnaNoTerminal",
+    logo: "/img/ana-no-terminal.png",
+    alt: "Logo da Ana no terminal",
+    content: "Aprendendo para ensinar, a melhor forma de programar",
+    url: "https://www.youtube.com/channel/UCjBDPBy_A8bUnyiXQrwyuxg/videos",
+    className: "-radius"
+  },
+  {
+    title: "#RocketSeat",
+    logo: "/img/rocketseat.jpg",
+    alt: "Logo da RocketSeat",
+    content:
+      "Cursos 100% online e gratuitos para você entrar com o pé direito nas tecnologias mais desejadas do mercado.",
+    url: "https://rocketseat.com.br/starter",
+    className: "-radius"
+  },
+  {
+    title: "#JornadaDaDev",
+    logo: "/img/jornada-do-dev.png",
+    alt: "Logo da Jornada do dev",
+    content:
+      "Node.js é um interpretador de código JavaScript que funciona do lado do servidor. Seu objetivo é ajudar programadores na criação de aplicações de alta escalabilidade...",
+    url: "https://jornadadodev.com.br/cursos/curso-de-nodejs",
+    className: "-radius"
+  },
+  {
+    title: "#Balta.io",
+    logo: "/img/baltaio.png",
+    alt: "Logo do Balta.io",
+    content:
+      "Começe a aprender de graça com o André Baltieri fundador do balta.io",
+    url: "https://balta.io/comece-de-graca",
+    className: "-radius"
+  },
+  {
+    title: "#DevMedia",
+    logo: "/img/devmedia.png",
+    alt: "Logo da DevMedia",
+    content:
+      "Esse não é de graça. Mas você pode dividir sua assinatura com uma galera e ganhar até 50% de desconto na assinatura do devmedia",
+    url: "https://www.devmedia.com.br/marketing/assinatura-grupo/",
     className: ""
   }
 ];
