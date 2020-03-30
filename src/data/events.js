e39e2f36a5f3c4106cafe7e2655c84206cdbd816
Fausto Blanco <fausto.level@gmail.com>
2020-03-30 12:31:21 -0300

    move to end of file

--- src/data/events.js
+++ src/data/events.js
@@ -9,14 +9,6 @@ export default [
     className: ""
   },
   {
-    title: "#QuarentenaFlutterando",
-    logo: "/img/flutterando.jpeg",
-    alt: "Logo da Flutterando",
-    content: "Muito conteúdo de #Flutter para todos os níveis e gostos, 100% free! Pode tambem entrar nos nossos canais Discord, Telegram e fórum pra sanar duvidas e aprender ajudando alguém.",
-    url: "https://linktr.ee/Flutterando",
-    className: "-radius"
-  },
-  {
     title: "#Quarentena333",
     logo: "/img/codigo-falado.png",
     alt: "Logo da CollabCode com o mascote Gueio",
@@ -242,6 +234,14 @@ export default [
     content:
       "Liberamos 8 cursos FREE para você passar por essa quarentena se preparando para voltar a 500%",
     url: "https://pages.upinside.com.br/ficaemcasa",
+    className: "-radius"
+  },
+  {
+    title: "#QuarentenaFlutterando",
+    logo: "/img/flutterando.jpeg",
+    alt: "Logo da Flutterando",
+    content: "Muito conteúdo de #Flutter para todos os níveis e gostos, 100% free! Pode tambem entrar nos nossos canais Discord, Telegram e fórum pra sanar duvidas e aprender ajudando alguém.",
+    url: "https://linktr.ee/Flutterando",
     className: "-radius"
   }
 ];
